df86cf7b788f962e32ef3dc12748382e1f9c188e
Maxime Guilbert <maximeguilbert123@gmail.com>
2024-11-18 11:27:25 +0100

    18/11 11:27

--- 2024.py
+++ 2024.py
@@ -4,24 +4,25 @@ def creer_grille_liste(n):
 
 import random
 
-def ajouter_5_aleatoire(grille):
+def ajouter_24_aleatoire(grille):
     """
     Ajoute un 5 dans une case vide (' ') choisie aléatoirement dans la grille,
     sans utiliser de liste en compréhension.
     """
     chiffre = [2,4]
-    apparition = chiffre[random.randint(0,1)]
+    apparition1 = random.choice(chiffre)
+    apparition2 = random.choice(chiffre)
     cases_vides = []
     for i in range(len(grille)):
         for j in range(len(grille[i])):
             if grille[i][j] == ' ':
                 cases_vides.append((i, j))
     
-
-    
-    
-    i, j = random.choice(cases_vides)
-    grille[i][j] = apparition
+    i, j= random.choice(cases_vides)
+    grille[i][j] = apparition1
+    cases_vides.remove((i,j))
+    i,j = random.choice(cases_vides)
+    grille[i][j] = apparition2
 
 
 def convertir_grille_en_affichage(grille):
@@ -34,27 +35,25 @@ def convertir_grille_en_affichage(grille):
 
     for ligne in grille:
         res += barre
-        res += ''.join(f"| {val}  " if val != ' ' else "|    " for val in ligne) + '|\n'
+        ligne_remplie = ""
+        for valeur in ligne:
+            if valeur != ' ':
+                ligne_remplie += f"| {valeur}  "
+            else:
+                ligne_remplie += "|    "
+        ligne_remplie += '|\n'
+        res += ''.join(ligne_remplie)
     res += barre
     return res
 
 grille = creer_grille_liste(4)
-ajouter_5_aleatoire(grille)
+ajouter_24_aleatoire(grille)
 
 grille_affichee = convertir_grille_en_affichage(grille)
 print(grille_affichee)
 
 
-#def creer_grille_vide(n):
-    #res = ''
-    #barre = '-' * n * 5 + '-' + '\n'
-    #case = '|    ' *n + '|' +'\n'
-    #for i in range(n):
-        #res += barre
-        #res += case 
-    #res += barre
-    #print(res)
-#creer_grille_vide(5)
+
 
 
 
